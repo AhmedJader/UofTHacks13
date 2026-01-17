23c5f4d64b33cd419bb9104799c735c401bd13f7
Ahmed <Ahmedjader837@gmail.com>
2026-01-17 02:40:42 -0500

    scaffolded project with neon db, clerk auth and next framework.

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import { createUserMessage, deleteUserMessage } from './actions'
+import { db } from './db'
+import { auth } from '@clerk/nextjs/server'
+
+export default async function Home() {
+  const { isAuthenticated, userId } = await auth()
+  if (!isAuthenticated) throw new Error('User not found')
+  const existingMessage = await db.query.UserMessages.findFirst({
+    where: (messages, { eq }) => eq(messages.user_id, userId),
+  })
+
+  return (
+    <main>
+      <h1>Neon + Clerk Example</h1>
+      {existingMessage ? (
+        <div>
+          <p>{existingMessage.message}</p>
+          <form action={deleteUserMessage}>
+            <button>Delete Message</button>
+          </form>
+        </div>
+      ) : (
+        <form action={createUserMessage}>
+          <input type="text" name="message" placeholder="Enter a message" />
+          <button>Save Message</button>
+        </form>
+      )}
+    </main>
+  )
+}
